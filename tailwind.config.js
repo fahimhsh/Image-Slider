fcf38e41d0dd2470e8b2d27ba8b87a56d03534f1
fahimhsh <fbchalai.420@gmail.com>
2024-05-17 07:34:35 +0600

    Added all Tailwind Classes and made the layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,24 @@
 module.exports = {
   content: ["./*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      maxWidth: {
+        "container-max-width": "1200px",
+      },
+      width: {
+        "container-width": "95%",
+        "image-width": "325px",
+      },
+      gap: {
+        "image-gap": "18px",
+      },
+      height: {
+        "image-height": "400px",
+      },
+      backgroundColor: {
+        "back-body": "#f1f4fd",
+      },
+    },
   },
   plugins: [],
-}
-
+};
